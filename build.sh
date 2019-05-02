398533d2ad0857d75b41979fc3d424fce7ea644e
Sergey Tihon <sergey.tihon@gmail.com>
2019-05-02 11:20:27 +0300

    fix for macOS build

--- build.sh
+++ build.sh
@@ -6,11 +6,11 @@ else
   dotnet tool install fake-cli --tool-path .fake --version 5.12.6
   dotnet tool install paket --tool-path .paket
 
-  .paket/paket restore
+  ./.paket/paket restore
   exit_code=$?
   if [ $exit_code -ne 0 ]; then
     exit $exit_code
   fi
   
- .fake/fake run build.fsx $@
+  ./.fake/fake run build.fsx $@
 fi

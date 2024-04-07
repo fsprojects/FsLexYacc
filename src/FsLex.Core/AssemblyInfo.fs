6452406759f2b9ac2f190438d7af35933658eed3
dawe <dawedawe@posteo.de>
2024-04-07 11:55:20 +0200

    sync AssemblyVersion/AssemblyFileVersion with RELEASE_NOTES

--- src/FsLex.Core/AssemblyInfo.fs
+++ src/FsLex.Core/AssemblyInfo.fs
@@ -5,13 +5,13 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("FsLex.Core")>]
 [<assembly: AssemblyProductAttribute("FsLexYacc")>]
 [<assembly: AssemblyDescriptionAttribute("FsLex/FsYacc lexer/parser generation tools")>]
-[<assembly: AssemblyVersionAttribute("11.0.1")>]
-[<assembly: AssemblyFileVersionAttribute("11.0.1")>]
+[<assembly: AssemblyVersionAttribute("11.2.0")>]
+[<assembly: AssemblyFileVersionAttribute("11.2.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "FsLex.Core"
     let [<Literal>] AssemblyProduct = "FsLexYacc"
     let [<Literal>] AssemblyDescription = "FsLex/FsYacc lexer/parser generation tools"
-    let [<Literal>] AssemblyVersion = "11.0.1"
-    let [<Literal>] AssemblyFileVersion = "11.0.1"
+    let [<Literal>] AssemblyVersion = "11.2.0"
+    let [<Literal>] AssemblyFileVersion = "11.2.0"
